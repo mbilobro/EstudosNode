6de5220743f6aa204b7ac4be974b7151b8e8507b
Matheus Bilobrovec <matheusbilobrovec@gmail.com>
2021-03-24 17:42:02 -0300

    Aula 7 - BackEnd com NodeJS

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/projects', (request, response) => {
+const {title, owner} = request.query;
+
+    console.log(title);
+    console.log(owner); 
+
+    return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    ]);
+});
+
+app.post('/projects', (request, response) =>{
+    const {title, owner} = request.body;
+
+    console.log(title);
+    console.log(owner);
+
+        
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.put('/projects/:id', (request, response) =>{
+    const {id} = request.params;
+
+    console.log(id);
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) =>{
+    const {id} = request.params;
+
+    console.log(id);
+    return response.json([
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+
+app.listen(3333, () => {
+    console.log('😋 Back-end started');
+});
